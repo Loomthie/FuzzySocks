92ca2ee441d61accd9e9e397d13bbcb38e7bdc11
Loomthie <mcfarldl@mail.uc.edu>
2022-02-08 14:36:11 -0500

    Added Requirements.txt and Setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='FuzzySocks',
+    version='0.1',
+    packages=['FuzzySocks'],
+    url='',
+    license='',
+    author='Loomthie',
+    author_email='mcfarldl@mail.uc.edu',
+    description='Don\'t you love some fuzzy socks!'
+)
